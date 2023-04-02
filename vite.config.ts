4520d9be64a6e941562dd0fabb255b90ecb0ab7c
Mohamed Adil <me@mohamedadil.com>
2023-04-02 14:41:01 +0200

    should generate site staticlly (#4)
    close #3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import solid from "solid-start/vite";
+// @ts-ignore
+import staticAdapter from "solid-start-static";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({ ssr: false })],
+  plugins: [solid({ adapter: staticAdapter() })],
 });
