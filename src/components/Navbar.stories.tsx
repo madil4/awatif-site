d9c506b35cafbb698aa43a59fcf8c5c56b1ae1cc
Mohamed Adil <me@mohamedadil.com>
2023-04-09 15:08:53 +0300

    should have a navbar component
    close #11

---
+++ src/components/Navbar.stories.tsx
@@ -0,0 +1,5 @@
+import { Navbar } from "./Navbar";
+
+export const Default = () => <Navbar />;
+
+export default { title: "Navbar" };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
