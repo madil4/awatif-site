578bd57245b5fda2021858d8bc88ba53702bd17c
Mohamed Adil <me@mohamedadil.com>
2023-04-29 21:58:02 +0200

    should update home image
    close #31

--- src/components/HomeUpper.tsx
+++ src/components/HomeUpper.tsx
@@ -1,19 +1,16 @@
 export const HomeUpper = () => {
   return (
     <section class="flex flex-col items-center md:flex-row mb-10">
-      <div class="w-full md:w-2/5 md:mr-5">
-        <h2 class="text-3xl">
+      <div class="md:w-1/2  md:pr-14">
+        <h2 class="text-3xl mb-5">
           Online 3D parametric (algorithmic) structural engineering software
         </h2>
-        <p class="mt-5">
+        <p class="mb-5">
           Design and analyze trusses, frames, and beams with ease using our
           modern structural engineering software
         </p>
       </div>
-      <iframe
-        class="w-full h-96 mt-5 md:w-3/5 md:mt-0"
-        src="https://awatif.co/examples/2d-truss/"
-      ></iframe>
+      <img class="md:w-1/2 rounded-md" src="../3d-truss-analysis.jpg" />
     </section>
   );
 };
