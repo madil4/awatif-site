2c326dd4bf3e4045a395d24344a432edbd63c354
Mohamed Adil <me@mohamedadil.com>
2023-05-11 22:27:03 +0200

    add pricing page (#40)

--- src/routes/pricing.tsx
+++ src/routes/pricing.tsx
@@ -1,14 +1,57 @@
 export default function Pricing() {
   return (
-    <p>
-      Pricing is still in progress. Please subscribe to our newsletter&#32;
-      <a
-        href="https://mohamedadil.us19.list-manage.com/subscribe?u=80eec59eb329b1c9c00258524&id=95cfe71596"
-        target="_blank"
-      >
-        here
-      </a>{" "}
-      to get notified when it is done.
-    </p>
+    <>
+      <h2 class="text-3xl font-bold text-center mb-16 lg:text-5xl">
+        Pricing Plans
+      </h2>
+
+      <div class="grid grid-cols-1 gap-6 mx-auto text-center mb-16 md:grid-cols-3">
+        <div class="overflow-hidden border-2 border-gray-100 rounded-md p-5">
+          <p class="mb-4 text-lg font-medium text-gray-500">Basic Plan</p>
+          <p class="text-6xl font-bold mb-8">$0</p>
+          <ul class="space-y-1">
+            <li class=" text-gray-900 lg:text-xl">
+              For experimentation and learning
+            </li>
+            <li class=" text-red-800 lg:text-l">Limited to bar elements</li>
+            <li class=" text-red-800 lg:text-l">
+              Limited to linear and static analysis
+            </li>
+          </ul>
+        </div>
+
+        <div class="overflow-hidden border-2 border-gray-100 rounded-md p-5">
+          <p class="mb-4 text-lg font-medium text-gray-500">Standard Plan</p>
+          <p class="text-6xl font-bold mb-8">$10</p>
+          <ul class="space-y-1">
+            <li class=" text-gray-900 lg:text-xl">
+              For day-to-day structural design
+            </li>
+            <li class=" text-green-800 lg:text-l">Bar and frame elements</li>
+            <li class=" text-red-800 lg:text-l">
+              Limited to linear and static analysis
+            </li>
+          </ul>
+        </div>
+
+        <div class="overflow-hidden border-2 border-gray-100 rounded-md p-5">
+          <p class="mb-4 text-lg font-medium text-gray-500">Advanced Plan</p>
+          <p class="text-6xl font-bold mb-8">$50</p>
+          <ul class="space-y-1">
+            <li class=" text-gray-900 lg:text-xl">For advanced simulations</li>
+            <li class=" text-green-800 lg:text-l">Bar and frame elements </li>
+            <li class=" text-green-800 lg:text-l">
+              Linear, non-linear, static, dynamic analysis
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <p class="text-sm text-center  text-gray-700 lg:w-1/2 lg:mx-auto mb-16">
+        Don't trust the cloud? Don't worry! We can set up the app locally on
+        your computer or server. You can reach me by dropping a message at
+        mohamed@awatif.co
+      </p>
+    </>
   );
 }
