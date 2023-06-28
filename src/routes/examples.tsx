57ab4f2d7bc40b29e2124fdb3c1916cd09c55cde
Mohamed Adil <me@mohamedadil.com>
2023-06-28 12:05:17 +0200

    add examples (#45)

--- src/routes/examples.tsx
+++ src/routes/examples.tsx
@@ -4,16 +4,117 @@ export default function Examples() {
   return (
     <>
       <Title>Awatif - Examples</Title>
-      <p>
-        Examples are still in progress. Please subscribe to our newsletter&#32;
-        <a
-          href="https://mohamedadil.us19.list-manage.com/subscribe?u=80eec59eb329b1c9c00258524&id=95cfe71596"
-          target="_blank"
-        >
-          here
-        </a>{" "}
-        to get notified when it is done.
-      </p>
+      <div class="grid grid-cols-4 gap-4">
+        <div class="card card-compact w-72 bg-neutral shadow-xl">
+          <figure>
+            <a href="https://app.awatif.co/?user_id=1e9e6f54-bc8d-4dd7-8554-ffa7124f8d81&slug=three-bars">
+              <img src="/examples/three-bars.jpg" alt="Three Bars" />
+            </a>
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Three Bars</h2>
+            <p>
+              By{" "}
+              <a
+                href="https://www.linkedin.com/in/madil4/"
+                target="_blank"
+                class="link"
+              >
+                Mohamed Adil
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="card card-compact w-72 bg-neutral shadow-xl">
+          <figure>
+            <a href="https://app.awatif.co/?user_id=1e9e6f54-bc8d-4dd7-8554-ffa7124f8d81&slug=building">
+              <img src="/examples/building.jpg" alt="Building" />
+            </a>
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Building</h2>
+            <p>
+              By{" "}
+              <a
+                href="https://www.linkedin.com/in/madil4/"
+                target="_blank"
+                class="link"
+              >
+                Mohamed Adil
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="card card-compact w-72 bg-neutral shadow-xl">
+          <figure>
+            <a href="https://app.awatif.co/?user_id=1e9e6f54-bc8d-4dd7-8554-ffa7124f8d81&slug=2d-truss">
+              <img src="/examples/2d-truss.jpg" alt="2D Truss" />
+            </a>
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">2D Truss</h2>
+            <p>
+              By{" "}
+              <a
+                href="https://www.linkedin.com/in/madil4/"
+                target="_blank"
+                class="link"
+              >
+                Mohamed Adil
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="card card-compact w-72 bg-neutral shadow-xl">
+          <figure>
+            <a href="https://app.awatif.co/?user_id=1e9e6f54-bc8d-4dd7-8554-ffa7124f8d81&slug=warehouse">
+              <img src="/examples/warehouse.jpg" alt="Warehouse" />
+            </a>
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Warehouse</h2>
+            <p>
+              By{" "}
+              <a
+                href="https://www.linkedin.com/in/parishith-jayan-985b199a/"
+                target="_blank"
+                class="link"
+              >
+                Parishith Jayan
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="card card-compact w-72 bg-neutral shadow-xl">
+          <figure>
+            <a href="https://app.awatif.co/?user_id=1e9e6f54-bc8d-4dd7-8554-ffa7124f8d81&slug=sphere">
+              <img src="/examples/sphere.jpg" alt="Sphere" />
+            </a>
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Sphere</h2>
+            <p>
+              By{" "}
+              <a
+                href="https://www.linkedin.com/in/mitchellrademaker/"
+                target="_blank"
+                class="link"
+              >
+                Mitchell Rademaker
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="card card-compact w-72 bg-neutral shadow-xl">
+          <figure>
+            <img src="/examples/add-yours.jpg" alt="Add Yours!" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Want to Contribute?</h2>
+            <p>Send me the link mohamed@awatif.co</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
